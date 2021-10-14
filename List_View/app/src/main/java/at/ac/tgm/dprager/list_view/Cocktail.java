b12d5c9a9d265b376727ea264be08ce67a08c28d
danielprager <dprager@student.tgm.ac.at>
2021-10-14 14:02:03 +0200

    CustomListAdapter hinzugefügt

--- List_View/app/src/main/java/at/ac/tgm/dprager/list_view/Cocktail.java
+++ List_View/app/src/main/java/at/ac/tgm/dprager/list_view/Cocktail.java
@@ -2,15 +2,46 @@ package at.ac.tgm.dprager.list_view;
 
 public class Cocktail {
 
+
     private String cocktail_image;
 
     private String cocktail_name;
-    private String aufwand;
+    private int aufwand;
 
-    public Cocktail(String cocktail_image, String cocktail_name, String aufwand) {
+    public Cocktail(String cocktail_image, String cocktail_name, int aufwand) {
         this.cocktail_image = cocktail_image;
         this.cocktail_name = cocktail_name;
         this.aufwand = aufwand;
+    }
+
+    public int getAufwand() {
+        return this.aufwand;
+    }
+
+    public void setAufwand(int aufwand) {
+        this.aufwand = aufwand;
+    }
+
+    public String getCocktailName() {
+        return this.cocktail_name;
+    }
+
+    public void setCocktail_name(String cocktail_name) {
+        this.cocktail_name = cocktail_name;
+
+    }
+
+    public String getCocktail_image() {
+        return cocktail_image;
+    }
+
+    public void setCocktail_image(String cocktail_image) {
+        this.cocktail_image = cocktail_image;
+    }
+
+    @Override
+    public String toString() {
+        return this.cocktail_name+" (Dauer: "+ this.aufwand+")";
     }
 
 
---
+++ List_View/app/src/main/java/at/ac/tgm/dprager/list_view/CustomListAdapter.java
@@ -0,0 +1,80 @@
+package at.ac.tgm.dprager.list_view;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.List;
+
+public class CustomListAdapter  extends BaseAdapter {
+
+    private List<Cocktail> listData;
+    private LayoutInflater layoutInflater;
+    private Context context;
+
+    public CustomListAdapter(Context aContext,  List<Cocktail> listData) {
+        this.context = aContext;
+        this.listData = listData;
+        layoutInflater = LayoutInflater.from(aContext);
+    }
+
+    @Override
+    public int getCount() {
+        return listData.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return listData.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = layoutInflater.inflate(R.layout.list_item_layout, null);
+            holder = new ViewHolder();
+            holder.imageView_Cocktail = (ImageView) convertView.findViewById(R.id.imageView_Cocktail);
+            holder.textView_Name = (TextView) convertView.findViewById(R.id.textView_Name);
+            holder.textView_Aufwand = (TextView) convertView.findViewById(R.id.textView_Aufwand);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        Cocktail cocktail = this.listData.get(position);
+        holder.textView_Name.setText(cocktail.getCocktailName());
+        holder.textView_Aufwand.setText("Population: " + cocktail.getAufwand());
+
+        int imageId = this.getMipmapResIdByName(cocktail.getCocktail_image());
+
+        holder.imageView_Cocktail.setImageResource(imageId);
+
+        return convertView;
+    }
+
+    // Find Image ID corresponding to the name of the image (in the directory mipmap).
+    public int getMipmapResIdByName(String resName)  {
+        String pkgName = context.getPackageName();
+        // Return 0 if not found.
+        int resID = context.getResources().getIdentifier(resName , "mipmap", pkgName);
+        Log.i("CustomListView", "Res Name: "+ resName+"==> Res ID = "+ resID);
+        return resID;
+    }
+
+    static class ViewHolder {
+        ImageView imageView_Cocktail;
+        TextView textView_Name;
+        TextView textView_Aufwand;
+
+    }
+
+}
