b788edd6a86265b2600e1f426119cc24e525a4eb
danielprager <dprager@student.tgm.ac.at>
2021-10-14 13:21:21 +0200

    ListView hinzugefügt und Cocktail Datei angefangen

---
+++ List_View/app/src/main/java/at/ac/tgm/dprager/list_view/Cocktail.java
@@ -0,0 +1,17 @@
+package at.ac.tgm.dprager.list_view;
+
+public class Cocktail {
+
+    private String cocktail_image;
+
+    private String cocktail_name;
+    private String aufwand;
+
+    public Cocktail(String cocktail_image, String cocktail_name, String aufwand) {
+        this.cocktail_image = cocktail_image;
+        this.cocktail_name = cocktail_name;
+        this.aufwand = aufwand;
+    }
+
+
+}
